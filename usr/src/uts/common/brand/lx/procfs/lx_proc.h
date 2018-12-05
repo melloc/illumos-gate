5fe4416dadf512114988384612ab64808550f66f
Cody Peter Mello <cody.mello@joyent.com>
2018-12-05 20:01:42 +0000

    OS-7427 Support Linux congestion control interfaces in LX zones

--- usr/src/uts/common/brand/lx/procfs/lx_proc.h
+++ usr/src/uts/common/brand/lx/procfs/lx_proc.h
@@ -237,6 +237,9 @@ typedef enum lxpr_nodetype {
 	LXPR_SYS_NET_IPV4_ICMP_EIB,	/* .../icmp_echo_ignore_broadcasts */
 	LXPR_SYS_NET_IPV4_IP_FORWARD,	/* .../net/ipv4/ip_forward */
 	LXPR_SYS_NET_IPV4_IP_LPORT_RANGE, /* .../net/ipv4/ip_local_port_range */
+	LXPR_SYS_NET_IPV4_TCP_CC_ALLOW,	/* .../tcp_allowed_congestion_control */
+	LXPR_SYS_NET_IPV4_TCP_CC_AVAIL,	/* .../tcp_available_congestion_control */
+	LXPR_SYS_NET_IPV4_TCP_CC_CURR,	/* .../tcp_congestion_control */
 	LXPR_SYS_NET_IPV4_TCP_FIN_TO,	/* /proc/sys/net/ipv4/tcp_fin_timeout */
 	LXPR_SYS_NET_IPV4_TCP_KA_INT,	/* .../net/ipv4/tcp_keepalive_intvl */
 	LXPR_SYS_NET_IPV4_TCP_KA_TIM,	/* .../net/ipv4/tcp_keepalive_time */
